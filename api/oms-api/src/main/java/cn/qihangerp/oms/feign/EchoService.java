bed055fa1d123386c30f616a8fa243403692b6a6
Richie <280645618@qq.com>
2025-04-24 20:43:31 +0800

    优化代码

--- api/oms-api/src/main/java/cn/qihangerp/oms/feign/EchoService.java
+++ api/oms-api/src/main/java/cn/qihangerp/oms/feign/EchoService.java
@@ -1,11 +1,11 @@
-package cn.qihangerp.sys.feign;
+package cn.qihangerp.oms.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@FeignClient(name = "tao-api")
+@FeignClient(name = "open-api")
 public interface EchoService {
     @GetMapping(value = "/test/na")
     String echo(@RequestHeader(name = "Authorization",required = true) String Token);
