2b97c0af3042c8c5aaa338b33d5198f01ad3b3d4
启航 <280645618@qq.com>
2026-05-23 11:53:22 +0800

    精简项目架构

--- api/erp-api/src/main/java/cn/qihangerp/erp/request/StockOutEntryGenerateBo.java
+++
@@ -1,8 +0,0 @@
-package cn.qihangerp.erp.request;
-
-import lombok.Data;
-
-@Data
-public class StockOutEntryGenerateBo {
-    private Long id;
-}
--- api/open-api/src/main/java/cn/qihangerp/open/request/ErpOrderCancelRequest.java
+++
@@ -1,9 +0,0 @@
-package cn.qihangerp.open.request;
-
-import lombok.Data;
-
-@Data
-public class ErpOrderCancelRequest {
-    private Long id;
-    private String cancelReason;
-}
