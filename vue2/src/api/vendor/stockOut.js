e81b9ada6c72c392bad94d630a812ac7410c5527
richie <280645618@qq.com>
2026-08-26 17:05:54 +0800

    移除vue2老版本

--- vue2/src/api/vendor/stockOut.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function createStockOut(data) {
-  return request({
-    url: 'api/wms-api/vendor/stock-out/create',
-    method: 'post',
-    data
-  })
-}
